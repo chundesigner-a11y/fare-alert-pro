61ed942e3e8fac48a346287e06a2f8d6a624f003
Tina Yang <tinayang.designer@gmail.com>
2026-09-22 07:27:52 +0000

    再調整底圖裁切，把飛機機頭往上帶出

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -79,7 +79,7 @@ export function Landing() {
           <img
             src={heroImage}
             alt="飛機穿越藍天白雲"
-            className="absolute inset-0 h-full w-full object-cover object-[75%_0%]"
+            className="absolute inset-0 h-full w-full object-cover object-[55%_10%]"
             loading="eager"
           />
 
